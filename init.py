26f9d25df28d8c49fcc7090c5a62f12284cef4e6
zsong <zsong30@gmail.com>
2022-05-02 15:02:07 +0800

    split pinyin

--- init.py
+++ init.py
@@ -1,5 +1,6 @@
 import read_text
 
+
 def init():
     list1 = []
 
@@ -7,6 +8,39 @@ def init():
 
     for x1 in x:
         y = x1.split(":")
-        list1.append((y[0], y[1]))
+        # list1.append((y[0], y[1]))
+        print(y[0])
+        print(y[1])
+        r = y[1]
+        r = r.strip('"')
+        z = r.split(" ")
 
+        c = [[], [], [], []]
+
+        for (i, z1) in enumerate(z):
+            lens = len(z1) - 1
+            if z1[0] == 'e':
+                c[i].append(z1[0])
+                c[i].append(z1[lens])
+                continue
+
+            if z1[0] == 'c' or z1[0] == 's' or z1[0] == 'z':
+                if z1[1] == 'h':
+                    c[i].append(z1[0:2])
+                    c[i].append(z1[2:lens])
+                    c[i].append(z1[lens])
+                else:
+                    c[i].append(z1[0])
+                    c[i].append(z1[1:lens])
+                    c[i].append(z1[lens])
+            else:
+                c[i].append(z1[0])
+                c[i].append(z1[1:lens])
+                c[i].append(z1[lens])
+
+        # print(c)
+
+        list1.append((y[0], c))
+
+    # print(list1[0])
     return list1
