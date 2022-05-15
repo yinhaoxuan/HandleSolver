c941d9abc27295824229f67994dd67a3332b0883
Haoxuan Yin <1723010279@qq.com>
2022-05-15 15:51:53 +0800

    unsuccess

--- init.py
+++ init.py
@@ -1,48 +1,64 @@
 import read_text
 import idioms
+import sounds
 
 def init():
-    polyphones_list = []
+    idiom_list = []
 
-    x = read_text.s.split(",")
+    # x = read_text.s.split(",")
+    #
+    # for x1 in x:
+    #     y = x1.split(":")
+    #     # idiom_list.append((y[0], y[1]))
+    #     # print(y[0])
+    #     # print(y[1])
+    #     r = y[1]
+    #     r = r.strip('"')
+    #     z = r.split(" ")
+    #
+    #     c = [[], [], [], []]
+    #
+    #     for (i, z1) in enumerate(z):
+    #         lens = len(z1) - 1
+    #         if z1[0] == 'e':
+    #             c[i].append(" ")
+    #             c[i].append(z1[0])
+    #             c[i].append(z1[lens])
+    #             continue
+    #
+    #         if z1[0] == 'c' or z1[0] == 's' or z1[0] == 'z':
+    #             if z1[1] == 'h':
+    #                 c[i].append(z1[0:2])
+    #                 c[i].append(z1[2:lens])
+    #                 c[i].append(z1[lens])
+    #             else:
+    #                 c[i].append(z1[0])
+    #                 c[i].append(z1[1:lens])
+    #                 c[i].append(z1[lens])
+    #         else:
+    #             c[i].append(z1[0])
+    #             c[i].append(z1[1:lens])
+    #             c[i].append(z1[lens])
+    #
+    #     # print(c)
+    #
+    #     idiom_list.append((y[0], c))
 
-    for x1 in x:
-        y = x1.split(":")
-        # polyphones_list.append((y[0], y[1]))
-        # print(y[0])
-        # print(y[1])
-        r = y[1]
-        r = r.strip('"')
-        z = r.split(" ")
+    diction = {} # 汉字到读音的map
+    for (snd, chars) in sounds.sounds.items():
+        for char in chars:
+            diction[char] = snd
 
-        c = [[], [], [], []]
+    for idiom in idioms.dict:
+        sound = []
+        for char in idiom:
+            print(char)
+            sound.append(diction[char])
+        idiom_list.append((idiom, sound))
 
-        for (i, z1) in enumerate(z):
-            lens = len(z1) - 1
-            if z1[0] == 'e':
-                c[i].append(" ")
-                c[i].append(z1[0])
-                c[i].append(z1[lens])
-                continue
 
-            if z1[0] == 'c' or z1[0] == 's' or z1[0] == 'z':
-                if z1[1] == 'h':
-                    c[i].append(z1[0:2])
-                    c[i].append(z1[2:lens])
-                    c[i].append(z1[lens])
-                else:
-                    c[i].append(z1[0])
-                    c[i].append(z1[1:lens])
-                    c[i].append(z1[lens])
-            else:
-                c[i].append(z1[0])
-                c[i].append(z1[1:lens])
-                c[i].append(z1[lens])
+    print(1)
+    print(idiom_list[0])
+    return idiom_list
 
-        # print(c)
-
-        polyphones_list.append((y[0], c))
-
-    # print(polyphones_list[0])
-    return polyphones_list
-
+init()
